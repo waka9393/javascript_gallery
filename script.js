52804a5aaf6f0c918d7948a9fad7a364cddcafa6
waka9393 <108017303+waka9393@users.noreply.github.com>
2023-10-18 15:19:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// JavaScript
+
+console.log('Hello world!');
+
+const main = document.querySelector('#main-image');
+const thumbs = document.querySelectorAll('.thumb');
+
+// 確認
+//console.log(main);
+//console.log(thumbs);
+
+const text = document.querySelector('#caption');
+
+
+thumbs.forEach(function(item, index) {
+  item.onmouseover = function() {
+    //console.log(this.dataset.image);
+    main.src = this.dataset.image;
+    text.textContent = this.title;
+  }
+});
